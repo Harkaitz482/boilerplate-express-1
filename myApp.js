c358dcfc9d7cc6b22efbf746db17e6708b8f9e80
Harkaitz482 <harkatrujillo@hotmail.com>
2024-01-12 18:06:14 +0000

    fafafafaf

--- myApp.js
+++ myApp.js
@@ -9,17 +9,19 @@ const absolutePath =__dirname + "/views/index.html"
 
 app.use("/public", express.static(__dirname + "/public"));
 
-if (process.env.VAR_NAME === "allCaps") {
-  response = "Hello World".toUpperCase();
-} else {
-  response = "Hello World";
-}
+
 
 
 app.get("/json", (req, res) => {
   res.json({
     message: "Hello json"
   });
+
+  if (process.env.VAR_NAME === "allCaps") {
+    response = "Hello World".toUpperCase();
+  } else {
+    response = "Hello World";
+  }
 });
 
 app.get("/", function(req, res) {
