3be503f3fc4fc77df2bf62a0b95aee3ff2d0b9d2
Harkaitz482 <harkatrujillo@hotmail.com>
2024-01-12 17:53:17 +0000

    Json

--- myApp.js
+++ myApp.js
@@ -10,6 +10,12 @@ const absolutePath =__dirname + "/views/index.html"
 app.use("/public", express.static(__dirname + "/public"));
 
 
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});
+
 app.get("/", function(req, res) {
      res.sendFile(absolutePath);
    });
