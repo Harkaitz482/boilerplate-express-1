b57b116b3dcec7fa2aa05dfe5af24538ea2d75b3
Harkaitz482 <harkatrujillo@hotmail.com>
2024-01-12 17:25:56 +0000

    app-use

--- myApp.js
+++ myApp.js
@@ -6,6 +6,10 @@ console.log("Hello World");
 
 const absolutePath =__dirname + "/views/index.html"
 
+
+app.use(express.static(__dirname + "/public"));
+
+
 app.get("/", function(req, res) {
      res.sendFile(absolutePath);
    });
